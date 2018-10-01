0da6050e2781bc9c66333421790f3c2cb6f7a36c
Kyle Wistrand <kyle.wistrand@gmail.com>
2018-10-01 10:10:37 -0700

    Initial Commit

---
+++ helloios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  helloios
+//
+//  Created by Kyle Wistrand on 10/1/18.
+//  Copyright © 2018 Kyle Wistrand. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
